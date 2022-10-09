206542faf72ea32da4305eaee5afa76915faba6b
carlosvendramini-bf <cvendramini@bishopfox.com>
2022-10-09 19:41:26 -0400

    wrote an interactive menu for Azure

--- azure/groups.go
+++ azure/groups.go
@@ -12,10 +12,7 @@ var ListSubscriptions = listSubscriptions
 
 func listSubscriptions() ([]string, error) {
 	var subs []string
-	subsClient, err := utils.GetSubscriptionsClient()
-	if err != nil {
-		return subs, err
-	}
+	subsClient := utils.GetSubscriptionsClient()
 	for page, err := subsClient.List(context.TODO()); page.NotDone(); err = page.Next() {
 		if err != nil {
 			return subs, err
@@ -31,10 +28,7 @@ var ListResourceGroups = listResourceGroups
 
 func listResourceGroups(subscription string) ([]string, error) {
 	var resourceGroups []string
-	rgClient, err := utils.GetResourceGroupsClient(subscription)
-	if err != nil {
-		return resourceGroups, err
-	}
+	rgClient := utils.GetResourceGroupsClient(subscription)
 	for page, err := rgClient.List(context.TODO(), "", nil); page.NotDone(); err = page.Next() {
 		if err != nil {
 			return resourceGroups, err
@@ -50,7 +44,6 @@ func listResourceGroups(subscription string) ([]string, error) {
 
 func PrintAvailableScope() (map[int]string, error) {
 	fmt.Println("Fetching available resource groups from your Azure CLI session...")
-	fmt.Println()
 
 	var index int
 	menu := make(map[int]string)
--- azure/groups_test.go
+++ azure/groups_test.go
@@ -3,6 +3,8 @@ package azure
 import (
 	"fmt"
 	"log"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -69,7 +71,103 @@ func TestPrintAvailableScope(t *testing.T) {
 				log.Println(err)
 			}
 			log.Println(menu)
+			fmt.Println()
 		})
 	}
-	fmt.Println()
+}
+
+func TestScopeSelection(t *testing.T) {
+	subtests := []struct {
+		name               string
+		ListSubscriptions  func() ([]string, error)
+		ListResourceGroups func(subscription string) ([]string, error)
+		userInput          string
+		expectedResult     []string
+	}{
+		{
+			name: "SubTest 1",
+			ListSubscriptions: func() ([]string, error) {
+				return []string{
+					"AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAA",
+					"BBBBBBBB-BBBB-BBBB-BBBB-BBBBBBBB",
+					"CCCCCCCC-CCCC-CCCC-CCCC-CCCCCCCC",
+				}, nil
+			},
+			ListResourceGroups: func(s string) ([]string, error) {
+				switch s {
+				case "AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAA":
+					return []string{"A1", "A2", "A3"}, nil
+				case "BBBBBBBB-BBBB-BBBB-BBBB-BBBBBBBB":
+					return []string{"B4", "B5", "B6"}, nil
+				case "CCCCCCCC-CCCC-CCCC-CCCC-CCCCCCCC":
+					return []string{"C7", "C8", "C9"}, nil
+				}
+				return []string{}, fmt.Errorf("no resource groups found for subscription: %s", s)
+			},
+			userInput:      "2,3,8",
+			expectedResult: []string{"A2", "A3", "C8"},
+		},
+		{
+			name: "SubTest 2",
+			ListSubscriptions: func() ([]string, error) {
+				return []string{
+					"AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAA",
+					"BBBBBBBB-BBBB-BBBB-BBBB-BBBBBBBB",
+					"CCCCCCCC-CCCC-CCCC-CCCC-CCCCCCCC",
+				}, nil
+			},
+			ListResourceGroups: func(s string) ([]string, error) {
+				switch s {
+				case "AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAA":
+					return []string{"A1", "A2", "A3"}, nil
+				case "BBBBBBBB-BBBB-BBBB-BBBB-BBBBBBBB":
+					return []string{"B4", "B5", "B6"}, nil
+				case "CCCCCCCC-CCCC-CCCC-CCCC-CCCCCCCC":
+					return []string{"C7", "C8", "C9"}, nil
+				}
+				return []string{}, fmt.Errorf("no resource groups found for subscription: %s", s)
+			},
+			userInput:      "1",
+			expectedResult: []string{"A1"},
+		},
+	}
+	for _, subtest := range subtests {
+		t.Run(subtest.name, func(t *testing.T) {
+			log.Println(subtest.name)
+			ListSubscriptions = subtest.ListSubscriptions
+			ListResourceGroups = subtest.ListResourceGroups
+			results, err := ScopeSelection(subtest.userInput)
+			if err != nil {
+				log.Println(err)
+			}
+			for i, r := range results {
+				if r != subtest.expectedResult[i] {
+					log.Fatalf(
+						"Expected result: %s, got %s",
+						strings.Join(subtest.expectedResult, ","),
+						strings.Join(results, ","),
+					)
+				}
+			}
+			log.Printf("Mocked user input: %s", subtest.userInput)
+			log.Printf("Matches expected result of: %s", strings.Join(results, ","))
+			fmt.Println()
+		})
+	}
+}
+
+func ScopeSelection(userInput string) ([]string, error) {
+	menu, err := PrintAvailableScope()
+	if err != nil {
+		log.Println(err)
+	}
+	var scope []string
+	for _, input := range strings.Split(userInput, ",") {
+		inputInt, err := strconv.Atoi(input)
+		if err != nil {
+			return scope, err
+		}
+		scope = append(scope, menu[int(inputInt)])
+	}
+	return scope, nil
 }
