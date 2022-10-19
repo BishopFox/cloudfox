8850384fc09f9943e270c83c54b651328bc0cc81
sethsec-bf <46326948+sethsec-bf@users.noreply.github.com>
2022-10-19 09:12:13 -0400

    pre 1..8.0 release cleanup

--- cli/aws.go
+++ cli/aws.go
@@ -876,7 +876,6 @@ var (
 
 				// Service and endpoint enum section
 				fmt.Printf("[%s] %s\n", cyan(emoji.Sprintf(":fox:cloudfox :fox:")), green("Gathering the info you'll want for your application & service enumeration needs."))
-				fmt.Printf("We are here!")
 				instances := aws.InstancesModule{
 					EC2Client:  ec2Client,
 					IAMClient:  iamClient,
