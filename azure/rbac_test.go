ae3ac75f23667130e44b2dadb95663b0f670937e
carlosvendramini-bf <cvendramini@bishopfox.com>
2022-11-20 15:37:23 -0500

    adjusted azure rbac command test template

--- azure/rbac_test.go
+++ azure/rbac_test.go
@@ -1,15 +1,57 @@
 package azure
 
 import (
+	"fmt"
+	"log"
 	"testing"
+
+	"github.com/BishopFox/cloudfox/globals"
+	"github.com/BishopFox/cloudfox/utils"
 )
 
 func TestRBACCommand(t *testing.T) {
+	fmt.Println()
+	fmt.Println("[test case] Azure RBAC Command")
+
+	// Test case parameters
+	utils.MockFileSystem(true)
+	subtests := []struct {
+		Name                    string
+		AzSubscriptionName      string
+		AzRGName                string
+		AzVerbosity             int
+		AzOutputFormat          string
+		usersTestFile           string
+		roleDefinitionsTestFile string
+		roleAssignmentsTestFile string
+	}{
+		{
+			Name:                    "basic acceptance",
+			AzSubscriptionName:      "",
+			AzRGName:                "",
+			AzVerbosity:             2,
+			AzOutputFormat:          "table",
+			usersTestFile:           "./test-data/users.json",
+			roleDefinitionsTestFile: "./test-data/role-definitions.json",
+			roleAssignmentsTestFile: "./test-data/role-assignments.json",
+		},
+	}
+	// Mocked functions to simulate Azure calls and responses
 	GetAzureADUsers = MockedGetAzureADUsers
 	GetRoleDefinitions = MockedGetRoleDefinitions
 	GetRoleAssignments = MockedGetRoleAssignments
 
-}
+	for _, s := range subtests {
+		fmt.Println()
+		fmt.Printf("[subtest] %s\n", s.Name)
+		globals.AAD_USERS_TEST_FILE = s.usersTestFile
+		globals.ROLE_DEFINITIONS_TEST_FILE = s.roleDefinitionsTestFile
+		globals.ROLE_ASSIGNMENTS_TEST_FILE = s.roleAssignmentsTestFile
 
-type InstancesClient struct {
+		var err error
+		// call the function to be tested here err = function...
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+	}
 }
