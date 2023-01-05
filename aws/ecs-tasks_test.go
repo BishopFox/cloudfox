c52a22c4f5de88e7a8d7c922e083a4ffea03e40e
sethsec-bf <46326948+sethsec-bf@users.noreply.github.com>
2023-01-05 12:20:53 -0500

    added acceptance test using t.Run to ecs-tasks_test.go

--- aws/ecs-tasks_test.go
+++ aws/ecs-tasks_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/BishopFox/cloudfox/internal"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
@@ -244,18 +245,48 @@ func (c *mockedDescribeTaskDefinitionInterface) DescribeTaskDefinition(ctx conte
 }
 
 func TestECSTasks(t *testing.T) {
-	m := ECSTasksModule{
-		AWSProfile:                      "default",
-		AWSRegions:                      []string{"us-east-1", "us-west-1"},
-		Caller:                          sts.GetCallerIdentityOutput{Arn: aws.String("arn:aws:iam::123456789012:user/cloudfox_unit_tests")},
-		SkipAdminCheck:                  true,
-		Goroutines:                      30,
-		DescribeNetworkInterfacesClient: &mockedDescribeNetworkInterfacesClient{},
-		DescribeTasksClient:             &mockedDescribeTasksClient{},
-		ListTasksClient:                 &mockedListTasksClient{},
-		ListClustersClient:              &mockedListclustersClient{},
-		DescribeTaskDefinitionClient:    &mockedDescribeTaskDefinitionInterface{},
-		//IAMSimulatePrincipalPolicyClient: &mockedDescribeTaskDefinitionsClient{},
+	subtests := []struct {
+		name            string
+		outputDirectory string
+		verbosity       int
+		testModule      ECSTasksModule
+		expectedResult  []MappedECSTask
+	}{
+		{
+			name:            "test1",
+			outputDirectory: ".",
+			verbosity:       2,
+			testModule: ECSTasksModule{
+
+				AWSProfile:                      "default",
+				AWSRegions:                      []string{"us-east-1", "us-west-1"},
+				Caller:                          sts.GetCallerIdentityOutput{Arn: aws.String("arn:aws:iam::123456789012:user/cloudfox_unit_tests")},
+				SkipAdminCheck:                  true,
+				Goroutines:                      30,
+				DescribeNetworkInterfacesClient: &mockedDescribeNetworkInterfacesClient{},
+				DescribeTasksClient:             &mockedDescribeTasksClient{},
+				ListTasksClient:                 &mockedListTasksClient{},
+				ListClustersClient:              &mockedListclustersClient{},
+				DescribeTaskDefinitionClient:    &mockedDescribeTaskDefinitionInterface{},
+				//IAMSimulatePrincipalPolicyClient: &mockedDescribeTaskDefinitionsClient{},
+			},
+			expectedResult: []MappedECSTask{{
+				Cluster:    "MyCluster",
+				ID:         "74de0355a10a4f979ac495c14EXAMPLE",
+				ExternalIP: "203.0.113.12",
+				Role:       "test123",
+			}},
+		},
 	}
-	m.ECSTasks("table", ".", 3)
+	internal.MockFileSystem(true)
+	for _, subtest := range subtests {
+		t.Run(subtest.name, func(t *testing.T) {
+			subtest.testModule.ECSTasks(subtest.testModule.OutputFormat, subtest.outputDirectory, subtest.verbosity)
+			for index, expectedTask := range subtest.expectedResult {
+				if expectedTask.Cluster != subtest.testModule.MappedECSTasks[index].Cluster {
+					log.Fatal("Cluster name does not match expected value")
+				}
+			}
+		})
+	}
 }
