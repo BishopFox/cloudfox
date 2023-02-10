2e8a454d044d064340b0d5dabe3dc27f27faeea8
carlosvendramini <5666957+carlosvendramini@users.noreply.github.com>
2023-02-10 13:06:22 -0700

    output redesign ready for testing in prod

--- internal/output2.go
+++ internal/output2.go
@@ -1,116 +1,260 @@
 package internal
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
+	"os"
+	"path"
 
+	"github.com/aquasecurity/table"
+	"github.com/fatih/color"
 	"github.com/spf13/afero"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
-type fileName string
-type fileContents string
-type statusMessage string
+// Used for file system mocking with Afero library. Set:
+// fileSystem = afero.NewOsFs() if not unit testing (code will use real file system) OR
+// fileSystem = afero.NewMemMapFs() for a mocked file system (when unit testing)
+var fileSystem = afero.NewOsFs()
 
-// Verbosity = 1 (Output and loot printed to file).
-// Verbosity = 2 (Output and loot printed to file, output printed screen).
-// Verbosity = 3 (Output and loot printed to file and screen).
+// Color functions
+var cyan = color.New(color.FgCyan).SprintFunc()
 
 type OutputClient struct {
 	Verbosity        int
 	CallingModule    string
 	PrefixIdentifier string
-	Base             BaseClient
+	Table            TableClient
 	Loot             LootClient
 }
 
-func (o *OutputClient) WriteFullOutput(header []string, body [][]string, lootFiles map[fileName][]fileContents) []statusMessage {
-	if o.Verbosity == 2 && o.Base.Format == "table" {
-		o.Base.printTabletoScreen(header, body)
+type TableClient struct {
+	Wrap          bool
+	DirectoryName string
+	TableFiles    []TableFile
+}
 
-	} else if o.Verbosity == 2 && o.Base.Format == "csv" {
-		o.Base.printCSVtoScreen(header, body)
-
-	} else if o.Verbosity == 3 && o.Base.Format == "table" {
-		o.Base.printTabletoScreen(header, body)
-		o.Loot.printLoottoScreen(lootFiles)
-
-	} else if o.Verbosity == 3 && o.Base.Format == "csv" {
-		o.Base.printCSVtoScreen(header, body)
-		o.Loot.printLoottoScreen(lootFiles)
-	}
-
-	outputFileTable := o.Base.createTableFile(o.Base.DirectoryName, o.Base.FileName)
-	outputFileCSV := o.Base.createCSVFile(o.Base.DirectoryName, o.Base.FileName)
-	lootFileList := o.Loot.createLootFiles(lootFiles)
-
-	var statusMessages []statusMessage
-	tableOutputFileStatus := o.Base.writeTableFile(header, body, outputFileTable)
-	CSVOutputFileStatus := o.Base.writeCSVFile(header, body, outputFileCSV)
-	lootFileStatuses := o.Loot.writeLootFiles(lootFiles, lootFileList)
-
-	statusMessages = append(statusMessages, tableOutputFileStatus, CSVOutputFileStatus)
-	statusMessages = append(statusMessages, lootFileStatuses...)
-	return statusMessages
+type TableFile struct {
+	Name             string
+	TableFilePointer afero.File
+	CSVFilePointer   afero.File
+	Header           []string
+	Body             [][]string
 }
 
 type LootClient struct {
 	DirectoryName string
+	LootFiles     []LootFile
 }
 
-func (b *LootClient) printLoottoScreen(lootFiles map[fileName][]fileContents) {
-	for fileName, fileContents := range lootFiles {
-		fmt.Printf("Printing contents of loot file %s", fileName)
-		for _, line := range fileContents {
-			fmt.Println(line)
+type LootFile struct {
+	Name        string
+	FilePointer afero.File
+	Contents    string
+}
+
+func (o *OutputClient) WriteFullOutput(tables []TableFile, lootFiles []LootFile) {
+	switch o.Verbosity {
+	case 2:
+		o.Table.printTablesToScreen(tables)
+	case 3:
+		o.Table.printTablesToScreen(tables)
+		fmt.Println()
+		o.Loot.printLoottoScreen(lootFiles)
+	}
+
+	o.Loot.createLootFiles(lootFiles)
+	lootOutputPaths := o.Loot.writeLootFiles()
+	o.Table.createTableFiles(tables)
+	tableOutputPaths := o.Table.writeTableFiles(tables)
+	o.Table.createCSVFiles()
+	csvOutputPaths := o.Table.writeCSVFiles()
+
+	var outputPaths []string
+	outputPaths = append(outputPaths, lootOutputPaths...)
+	outputPaths = append(outputPaths, tableOutputPaths...)
+	outputPaths = append(outputPaths, csvOutputPaths...)
+
+	for _, path := range outputPaths {
+		fmt.Printf("[%s][%s] Output written to %s\n", cyan(o.CallingModule), cyan(o.PrefixIdentifier), path)
+	}
+}
+
+func (l *LootClient) printLoottoScreen(lootFiles []LootFile) {
+	for _, file := range lootFiles {
+		fmt.Println(file.Contents)
+	}
+}
+
+func (l *LootClient) createLootFiles(lootFiles []LootFile) {
+	l.LootFiles = lootFiles
+
+	for i, file := range l.LootFiles {
+		if l.DirectoryName == "" {
+			l.DirectoryName = "."
 		}
-	}
-}
-
-func (b *LootClient) createLootFiles(lootFiles map[fileName][]fileContents) []afero.File {
-	for fileName, _ := range lootFiles {
-		fmt.Printf("Creating file %s", fileName)
-	}
-	// This will return a pointer to the created file
-	return nil
-}
-
-func (b *LootClient) writeLootFiles(lootFiles map[fileName][]fileContents, outputFiles []afero.File) []statusMessage {
-	for fileName, fileContents := range lootFiles {
-		fmt.Printf("Writing contents of loot file %s", fileName)
-		for _, line := range fileContents {
-			fmt.Println(line)
+		if _, err := fileSystem.Stat(l.DirectoryName); os.IsNotExist(err) {
+			err = fileSystem.MkdirAll(l.DirectoryName, 0700)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
+		if file.Name == "" {
+			log.Fatalf("error creating loot file: no file name was specified")
+		}
+
+		l.LootFiles[i].Name = fmt.Sprintf("%s.txt", file.Name)
+
+		filePointer, err := fileSystem.OpenFile(path.Join(l.DirectoryName, l.LootFiles[i].Name), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+		if err != nil {
+			log.Fatalf("error creating output file: %s", err)
+		}
+
+		l.LootFiles[i].FilePointer = filePointer
 	}
-	return []statusMessage{}
 }
 
-type BaseClient struct {
-	WrapTable        bool
-	Format           string
-	PrefixIdentifier string
-	FileName         string
-	DirectoryName    string
+func (l *LootClient) writeLootFiles() []string {
+	var fullFilePaths []string
+	for _, file := range l.LootFiles {
+		contents := []byte(file.Contents)
+		fullPath := path.Join(l.DirectoryName, file.Name)
+
+		err := os.WriteFile(fullPath, contents, 0644)
+		if err != nil {
+			log.Fatalf("error writing loot file %s: %s", file.Name, err)
+		}
+		fullFilePaths = append(fullFilePaths, fullPath)
+	}
+	return fullFilePaths
 }
 
-func (b *BaseClient) printTabletoScreen(header []string, body [][]string) {
+func (b *TableClient) printTablesToScreen(tableFiles []TableFile) {
+	for _, tf := range tableFiles {
+		standardColumnWidth := 1000
+		t := table.New(os.Stdout)
 
+		if b.Wrap {
+			terminalWidth, _, err := terminal.GetSize(int(os.Stdout.Fd()))
+			if err != nil {
+				fmt.Printf("error getting terminal size: %s, please set the --wrap flag to false\n", err)
+				return
+			}
+			columnCount := len(tf.Header)
+			// The offset value was defined by trial and error to get the best wrapping
+			trialAndErrorOffset := 1
+			standardColumnWidth = terminalWidth / (columnCount + trialAndErrorOffset)
+		}
+
+		t.SetColumnMaxWidth(standardColumnWidth)
+		t.SetHeaders(tf.Header...)
+		t.AddRows(tf.Body...)
+		t.SetHeaderStyle(table.StyleBold)
+		t.SetRowLines(false)
+		t.SetLineStyle(table.StyleCyan)
+		t.SetDividers(table.UnicodeRoundedDividers)
+		t.SetAlignment(table.AlignLeft)
+		t.Render()
+	}
 }
 
-func (b *BaseClient) printCSVtoScreen(header []string, body [][]string) {
+func (b *TableClient) createTableFiles(files []TableFile) {
+	b.TableFiles = files
 
+	for i, file := range b.TableFiles {
+		if b.DirectoryName == "" {
+			b.DirectoryName = "."
+		}
+
+		tableDirectory := path.Join(b.DirectoryName, "table")
+
+		if _, err := fileSystem.Stat(tableDirectory); os.IsNotExist(err) {
+			err = fileSystem.MkdirAll(tableDirectory, 0700)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		if file.Name == "" {
+			log.Fatalf("error creating table file: no file name was specified")
+		}
+
+		fileNameWithExt := fmt.Sprintf("%s.txt", file.Name)
+
+		filePointer, err := fileSystem.OpenFile(path.Join(tableDirectory, fileNameWithExt), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+		if err != nil {
+			log.Fatalf("error creating table file: %s", err)
+		}
+
+		b.TableFiles[i].TableFilePointer = filePointer
+	}
 }
 
-func (b *BaseClient) createTableFile(outputDirectory, fileName string) afero.File {
-	return nil
+func (b *TableClient) writeTableFiles(files []TableFile) []string {
+	var fullFilePaths []string
+
+	for _, file := range b.TableFiles {
+		t := table.New(file.TableFilePointer)
+
+		t.SetHeaders(file.Header...)
+		t.AddRows(file.Body...)
+		t.SetRowLines(false)
+		t.SetDividers(table.UnicodeRoundedDividers)
+		t.SetAlignment(table.AlignLeft)
+		t.Render()
+
+		fullPath := path.Join(b.DirectoryName, file.Name)
+		fullFilePaths = append(fullFilePaths, fullPath)
+	}
+
+	return fullFilePaths
 }
 
-func (b *BaseClient) writeTableFile(header []string, body [][]string, outputFile afero.File) statusMessage {
-	return statusMessage("")
+func (b *TableClient) createCSVFiles() {
+	for i, file := range b.TableFiles {
+		if b.DirectoryName == "" {
+			b.DirectoryName = "."
+		}
+
+		csvDirectory := path.Join(b.DirectoryName, "csv")
+
+		if _, err := fileSystem.Stat(csvDirectory); os.IsNotExist(err) {
+			err = fileSystem.MkdirAll(csvDirectory, 0700)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		if file.Name == "" {
+			log.Fatalf("error creating csv file: no file name was specified")
+		}
+
+		fileNameWithExt := fmt.Sprintf("%s.csv", file.Name)
+
+		filePointer, err := fileSystem.OpenFile(path.Join(csvDirectory, fileNameWithExt), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+		if err != nil {
+			log.Fatalf("error creating csv file: %s", err)
+		}
+
+		b.TableFiles[i].CSVFilePointer = filePointer
+	}
 }
 
-func (b *BaseClient) createCSVFile(outputDirectory, fileName string) afero.File {
-	return nil
-}
+func (b *TableClient) writeCSVFiles() []string {
+	var fullFilePaths []string
 
-func (b *BaseClient) writeCSVFile(header []string, body [][]string, outputFile afero.File) statusMessage {
-	return statusMessage("")
+	for _, file := range b.TableFiles {
+		csvWriter := csv.NewWriter(file.CSVFilePointer)
+		csvWriter.Write(file.Header)
+		for _, row := range file.Body {
+			csvWriter.Write(row)
+		}
+		csvWriter.Flush()
+
+		fullPath := path.Join(b.DirectoryName, file.Name)
+		fullFilePaths = append(fullFilePaths, fullPath)
+	}
+
+	return fullFilePaths
 }
--- internal/output2_test.go
+++ internal/output2_test.go
@@ -1,38 +1,71 @@
 package internal
 
 import (
+	"fmt"
+	"path/filepath"
 	"testing"
+
+	"github.com/BishopFox/cloudfox/globals"
 )
 
 func TestOutput2(t *testing.T) {
-	outputFileHeader := []string{"Service", "Status"}
 
-	outputFileBody := [][]string{
-		{"IAM", "Active"},
-		{"EC2", "Not Active"},
-		{"Lambda", "Active"},
-		{"S3", "Not Active"},
+	tables := []TableFile{
+		{
+			Name:   "TableFile1",
+			Header: []string{"Service", "Status"},
+			Body: [][]string{
+				{"IAM", "Active"},
+				{"EC2", "Not Active"},
+				{"Lambda", "Active"},
+				{"S3", "Not Active"},
+			},
+		},
+		{
+			Name:   "TableFile2",
+			Header: []string{"Resource", "Condition"},
+			Body: [][]string{
+				{"ACR", "Good"},
+				{"AKS", "Bad"},
+				{"Storage Account", "Bad"},
+				{"Databricks", "Good"},
+			},
+		},
 	}
 
-	lootFiles := map[fileName][]fileContents{
-		"FileName1": {"line1", "line2"},
-		"FileName2": {"line1", "line2", "line3", "line4"},
+	lootFiles := []LootFile{
+		{
+			Name:        "loot1",
+			FilePointer: nil,
+			Contents:    "This is a loot file\nline1\nline2\n",
+		},
+		{
+			Name:        "loot2",
+			FilePointer: nil,
+			Contents:    "This is a loot file\nline1\nline2\nline3\nline4\n",
+		},
+		{
+			Name:        "loot3",
+			FilePointer: nil,
+			Contents:    "This is a loot file\nline1\nline2\nline3\n",
+		},
 	}
 
 	o := OutputClient{
-		Verbosity:        2,
+		Verbosity:        3,
 		CallingModule:    "testModule",
-		PrefixIdentifier: "customIdentifier",
-		Base: BaseClient{
-			WrapTable:     true,
-			Format:        "table",
-			FileName:      "callingModule",
-			DirectoryName: "baseOutputDirectory",
+		PrefixIdentifier: "DEV",
+		Table: TableClient{
+			Wrap:          false,
+			DirectoryName: filepath.Join(globals.CLOUDFOX_BASE_DIRECTORY, globals.AZ_DIR_BASE),
 		},
 		Loot: LootClient{
-			DirectoryName: "lootOutputDirectory",
+			DirectoryName: filepath.Join(globals.CLOUDFOX_BASE_DIRECTORY, globals.AZ_DIR_BASE, "loot"),
+			LootFiles:     nil,
 		},
 	}
 
-	o.WriteFullOutput(outputFileHeader, outputFileBody, lootFiles)
+	MockFileSystem(false)
+	fmt.Printf("Verbose level: %d", o.Verbosity)
+	o.WriteFullOutput(tables, lootFiles)
 }
