f6f78efd42de5aba6cb1fd2a213a3ea1200198df
Bastien Faure <bfaure@bishopfox.com>
2023-10-30 22:47:29 -0700

    Moved version information to its own file

--- globals/utils.go
+++ globals/utils.go
@@ -1,6 +1,12 @@
 package globals
 
+import (
+	_ "embed"
+)
+
 const CLOUDFOX_USER_AGENT = "cloudfox"
 const CLOUDFOX_LOG_FILE_DIR_NAME = ".cloudfox"
 const CLOUDFOX_BASE_DIRECTORY = "cloudfox-output"
 const LOOT_DIRECTORY_NAME = "loot"
+//go:embed VERSION
+var CLOUDFOX_VERSION string
