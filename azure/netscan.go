465f08b53068f87a91d5900dcfcd3ac067f15f30
Bastien Faure <bfaure@bishopfox.com>
2023-11-08 16:22:53 -0800

    Azure netscan: reduce list of network targets by filtering running vms, included in target subnets

--- azure/netscan.go
+++ azure/netscan.go
@@ -5,11 +5,14 @@ package azure
 import (
 	"github.com/BishopFox/cloudfox/internal"
 	"github.com/Azure/azure-sdk-for-go/profiles/latest/network/mgmt/network"
+	"github.com/Azure/azure-sdk-for-go/profiles/latest/compute/mgmt/compute"
 	"github.com/Azure/go-autorest/autorest/azure"
 	"strings"
+	"github.com/aws/smithy-go/ptr"
 	"context"
 	"path/filepath"
 	"fmt"
+	"net"
 	"os"
 )
 
@@ -55,6 +58,9 @@ func (m *AzNetScanModule) processVM(subscriptionID string, resourceGroupName str
 	computeClient := internal.GetVirtualMachinesClient(subscriptionID)
 	vm, err := computeClient.Get(context.TODO(), resourceGroupName, VMName, "")
 	targetSubnets := []string{}
+	targetSubnetsAlt := make(map[string][]string)
+	vmSubscriptionIDs := make(map[string]bool)
+	vmSubscriptionIDs[subscriptionID] = true
 	if err != nil {
 		m.Log.Errorf([]string{VMName}, "Could not fetch virtual machine details")
 		return
@@ -103,10 +109,12 @@ func (m *AzNetScanModule) processVM(subscriptionID string, resourceGroupName str
 					for _, subnet := range *vnet.VirtualNetworkPropertiesFormat.Subnets {
 						m.Log.Infof([]string{VMName, *nic.Name, *ipConfig.PrivateIPAddress}, "Potential target : %s", *subnet.SubnetPropertiesFormat.AddressPrefix)
 						targetSubnets = append(targetSubnets, *subnet.SubnetPropertiesFormat.AddressPrefix)
+						targetSubnetsAlt[*subnet.SubnetPropertiesFormat.AddressPrefix] = []string{}
 					}
 					for _, peering := range *vnet.VirtualNetworkPropertiesFormat.VirtualNetworkPeerings {
 						remoteVnetID := *peering.VirtualNetworkPeeringPropertiesFormat.RemoteVirtualNetwork.ID
 						remoteVnetResource, _ := azure.ParseResourceID(remoteVnetID)
+						vmSubscriptionIDs[remoteVnetResource.SubscriptionID] = true
 						if !*peering.VirtualNetworkPeeringPropertiesFormat.AllowForwardedTraffic {
 							m.Log.Warnf([]string{VMName, *nic.Name, *ipConfig.PrivateIPAddress, *subnet.Name}, "Peering to Virtual Network %s is disabled", remoteVnetResource.ResourceName)
 						} else {
@@ -114,13 +122,53 @@ func (m *AzNetScanModule) processVM(subscriptionID string, resourceGroupName str
 								remoteVnetResource.ResourceName, stringAndArrayToString(nil, peering.VirtualNetworkPeeringPropertiesFormat.RemoteAddressSpace.AddressPrefixes, " "))
 							for _, addressPrefix := range *peering.VirtualNetworkPeeringPropertiesFormat.RemoteAddressSpace.AddressPrefixes {
 								targetSubnets = append(targetSubnets, addressPrefix)
+								targetSubnetsAlt[addressPrefix] = []string{}
 							}
 						}
 					}
 
 				}
 			}
-		m.writeScanFile(VMName, targetSubnets)
+		m.Log.Info([]string{VMName}, "Narrowing down potential target subnets with running compute instances...")
+		var vms []compute.VirtualMachine
+		for subscriptionID, _ := range vmSubscriptionIDs {
+			subscriptionVMs, err := getComputeVMsPerSubscription(subscriptionID)
+			if err != nil {
+				m.Log.Warnf([]string{VMName}, "Unable to enumerate VMs in subscription %s", subscriptionID)
+				continue
+			}
+			vms = append(vms, subscriptionVMs...)
+		}
+		for i, vm := range vms {
+			fmt.Printf("Processing machine %d%%\r", 100*i/len(vms))
+			vmResource, _ := azure.ParseResourceID(*vm.ID)
+			vmDetails, err := getComputeVmInstanceView(subscriptionID, vmResource.ResourceGroup, ptr.ToString(vm.Name))
+			if err != nil {
+				m.Log.Warnf([]string{VMName}, "Unable to fetch VM %s details", ptr.ToString(vm.Name))
+				continue
+			}
+			if vmDetails.VirtualMachineProperties != nil && vmDetails.VirtualMachineProperties.InstanceView != nil {
+				for _, status := range *vmDetails.VirtualMachineProperties.InstanceView.Statuses {
+					if *status.Code == "PowerState/running" {
+						privateIPs, _ := getIPs(subscriptionID, vmResource.ResourceGroup, vm)
+						for targetSubnet, _ := range targetSubnetsAlt {
+							_, ipnet, _ := net.ParseCIDR(targetSubnet)
+							for _, privateIP := range privateIPs {
+								ip := net.ParseIP(privateIP)
+								if ipnet.Contains(ip) {
+									targetSubnetsAlt[targetSubnet] = append(targetSubnetsAlt[targetSubnet], privateIP)
+								}
+							}
+						}
+						break
+					}
+				}
+			}
+		}
+		for targetSubnet, targets := range targetSubnetsAlt {
+			m.Log.Infof([]string{VMName}, "%s subnet has %s addresses", targetSubnet, len(targets))
+		}
+		m.writeScanFile(VMName, targetSubnetsAlt)
 		} else {
 			m.Log.Errorf(nil, "Virtual Machine %s has no network interface", VMName)
 			return
@@ -128,7 +176,27 @@ func (m *AzNetScanModule) processVM(subscriptionID string, resourceGroupName str
 	}
 }
 
-func (m *AzNetScanModule) writeScanFile(vm string, subnets []string) error {
+func (m *AzNetScanModule) filterVM(vm compute.VirtualMachine) {
+
+}
+func getComputeVMsPerSubscription(subscriptionID string) ([]compute.VirtualMachine, error) {
+	computeClient := internal.GetVirtualMachinesClient(subscriptionID)
+	var vms []compute.VirtualMachine
+
+	for _, rg := range GetResourceGroups(subscriptionID) {
+		for page, err := computeClient.List(context.TODO(), ptr.ToString(rg.Name), ""); page.NotDone(); page.Next() {
+			if err != nil {
+				return nil, fmt.Errorf("could not enumerate resource group %s. %s", rg, err)
+			} else {
+
+				vms = append(vms, page.Values()...)
+			}
+		}
+	}
+	return vms, nil
+}
+
+func (m *AzNetScanModule) writeScanFile(vm string, targetSubnetsAlt map[string][]string) error {
 	lootDirectory := filepath.Join(m.AzClient.AzOutputDirectory, "loot")
 	lootFilePath := filepath.Join(lootDirectory, fmt.Sprintf("scan-from-%s.txt", vm))
 	err := os.MkdirAll(lootDirectory, os.ModePerm)
@@ -142,10 +210,19 @@ func (m *AzNetScanModule) writeScanFile(vm string, subnets []string) error {
 	}
 	defer file.Close()
 
-	for _, subnet := range subnets {
-		_, err := file.WriteString(fmt.Sprintf("nmap -p- --max-retries 1 -T4 -Pn -sV -sC -oA %s %s\n", strings.Replace(subnet, "/", "_", -1), subnet))
+	for targetSubnet, targets := range targetSubnetsAlt {
+		cleanSubnet := strings.Replace(targetSubnet, "/", "_", -1)
+		_, err := file.WriteString(fmt.Sprintf("nmap -p- --max-retries 1 -T4 -Pn -sV -sC -oA %s -iL %s.lst\n", cleanSubnet, cleanSubnet))
 		if err != nil {
 			return err
+		}
+		targetsFile, err := os.Create(filepath.Join(lootDirectory, fmt.Sprintf("%s.lst", cleanSubnet)))
+		if err != nil {
+			return err
+		}
+		defer targetsFile.Close()
+		for _, target := range targets {
+			targetsFile.WriteString(fmt.Sprintf("%s\n", target))
 		}
 	}
 	m.Log.Successf(nil, "Scan file written to %s", lootFilePath)
