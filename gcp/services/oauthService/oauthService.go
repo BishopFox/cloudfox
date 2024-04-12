94628aae730d94154fa132bbe7cd42634c1b7428
David <dbravo@bishopfox.com>
2024-04-12 01:12:19 +0000

    i cant spel

--- gcp/services/oauthService/oauthService.go
+++ gcp/services/oauthService/oauthService.go
@@ -48,7 +48,7 @@ func (s *OAuthService) WhoAmI() (*Principal, error) {
 
 	tokenInfo, err := queryTokenInfo(token.AccessToken)
 	if err != nil {
-		return nil, fmt.Errorf(fmt.Sprintf("failed to retreive metada of the token with error: %s", err.Error()))
+		return nil, fmt.Errorf(fmt.Sprintf("failed to retrieve metada of the token with error: %s", err.Error()))
 	}
 	// Split the scope string into a slice of strings.
 	scopes := strings.Split(tokenInfo.Scope, " ")
